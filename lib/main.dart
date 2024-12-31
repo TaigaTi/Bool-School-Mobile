6a4eda61bd248b0a4c175658637ef9dc9818f213
TaigaTi <tarikabirch@gmail.com>
2024-12-31 10:01:43 -0400

    Created smaller components

--- lib/main.dart
+++ lib/main.dart
@@ -90,172 +90,7 @@ class Quiz extends StatelessWidget {
                   ]),
                 ]),
               ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 20, left: 30, right: 30),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset(
-                              'assets/left-arrow.png',
-                              height: 50,
-                            ),
-                            Image.asset(
-                              'assets/algebra.png',
-                              height: 50,
-                            ),
-                            Image.asset(
-                              'assets/right-arrow.png',
-                              height: 50,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 5),
-                        child: Text(
-                          'Question 1',
-                          style: TextStyle(
-                            color: Colors.blue.shade800,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 250,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 15),
-                          child: Center(
-                            child: Opacity(
-                              opacity: 0.8,
-                              child: Text(
-                                'Boolean algebra is a branch of algebra that was formulated by _______',
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(25),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                fixedSize: Size.fromWidth(400),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                ),
-                                backgroundColor: const Color.fromARGB(
-                                    71, 158, 158, 158), // Background color
-                              ),
-                              child: Text(
-                                'Rihanna Fenty',
-                                style: TextStyle(
-                                    color: Colors.blue.shade800,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                fixedSize: Size.fromWidth(400),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                ),
-                                backgroundColor: const Color.fromARGB(
-                                    71, 158, 158, 158), // Background color
-                              ),
-                              child: Text(
-                                'John Boole',
-                                style: TextStyle(
-                                    color: Colors.blue.shade800,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                fixedSize: Size.fromWidth(400),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                ),
-                                backgroundColor: const Color.fromARGB(
-                                    71, 158, 158, 158), // Background color
-                              ),
-                              child: Text(
-                                'George Boole',
-                                style: TextStyle(
-                                    color: Colors.blue.shade800,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              style: TextButton.styleFrom(
-                                fixedSize: Size.fromWidth(400),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                ),
-                                backgroundColor: const Color.fromARGB(
-                                    71, 158, 158, 158), // Background color
-                              ),
-                              child: Text(
-                                'George Washington',
-                                style: TextStyle(
-                                    color: Colors.blue.shade800,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 15),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  TextButton(
-                                    onPressed: () {},
-                                    style: TextButton.styleFrom(
-                                      side: BorderSide(
-                                          color: Colors.blue.shade900),
-                                      fixedSize: Size.fromWidth(200),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(5)),
-                                      ),
-                                      backgroundColor: const Color.fromARGB(
-                                          71, 158, 158, 158),
-                                    ),
-                                    child: Text(
-                                      'Evaluate',
-                                      style: TextStyle(
-                                          color: Colors.blue.shade900),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              QuestionCard(),
             ],
           ),
           Padding(
@@ -292,6 +127,135 @@ class Quiz extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class QuestionCard extends StatelessWidget {
+  const QuestionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 30, right: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/left-arrow.png',
+                    height: 50,
+                  ),
+                  Image.asset(
+                    'assets/algebra.png',
+                    height: 50,
+                  ),
+                  Image.asset(
+                    'assets/right-arrow.png',
+                    height: 50,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 5),
+              child: Text(
+                'Question 1',
+                style: TextStyle(
+                  color: Colors.blue.shade800,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 250,
+              child: Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Center(
+                  child: Opacity(
+                    opacity: 0.8,
+                    child: Text(
+                      'Boolean algebra is a branch of algebra that was formulated by _______',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  QuestionChoice(text: 'Rihanna Fenty'),
+                  QuestionChoice(text: 'John Doe'),
+                  QuestionChoice(text: 'George Boole'),
+                  QuestionChoice(text: 'George Washington'),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            side: BorderSide(color: Colors.blue.shade900),
+                            fixedSize: Size.fromWidth(200),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                            ),
+                            backgroundColor:
+                                const Color.fromARGB(71, 158, 158, 158),
+                          ),
+                          child: Text(
+                            'Evaluate',
+                            style: TextStyle(color: Colors.blue.shade900),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionChoice extends StatelessWidget {
+  final String text;
+  const QuestionChoice({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: TextButton.styleFrom(
+        fixedSize: Size.fromWidth(400),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        backgroundColor:
+            const Color.fromARGB(71, 158, 158, 158), // Background color
+      ),
+      child: Text(
+        text,
+        style:
+            TextStyle(color: Colors.blue.shade800, fontWeight: FontWeight.bold),
       ),
     );
   }
