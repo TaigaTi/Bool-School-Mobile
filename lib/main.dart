c7fe40403c99e50f2502549b30b0d33afd38b51b
TaigaTi <tarikabirch@gmail.com>
2024-12-31 10:04:43 -0400

    Separated navbar logic

--- lib/main.dart
+++ lib/main.dart
@@ -93,39 +93,7 @@ class Quiz extends StatelessWidget {
               QuestionCard(),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/home.png',
-                  height: 40,
-                ),
-                Opacity(
-                  opacity: 0.5,
-                  child: Image.asset(
-                    'assets/learn.png',
-                    height: 40,
-                  ),
-                ),
-                Opacity(
-                  opacity: 0.5,
-                  child: Image.asset(
-                    'assets/quiz.png',
-                    height: 40,
-                  ),
-                ),
-                Opacity(
-                  opacity: 0.5,
-                  child: Image.asset(
-                    'assets/profile.png',
-                    height: 40,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          NavBar(),
         ],
       ),
     );
@@ -256,6 +224,47 @@ class QuestionChoice extends StatelessWidget {
         text,
         style:
             TextStyle(color: Colors.blue.shade800, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/home.png',
+            height: 40,
+          ),
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/learn.png',
+              height: 40,
+            ),
+          ),
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/quiz.png',
+              height: 40,
+            ),
+          ),
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/profile.png',
+              height: 40,
+            ),
+          ),
+        ],
       ),
     );
   }
