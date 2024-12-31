a32d74d8b08460f299b00d8b1d6d912a08202e5a
TaigaTi <tarikabirch@gmail.com>
2024-12-31 09:43:14 -0400

    Added question box to quiz page

--- lib/main.dart
+++ lib/main.dart
@@ -54,64 +54,241 @@ class Quiz extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 40,
-              left: 20,
-              right: 20,
-              bottom: 10,
-            ),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Column(children: [
-                Image.asset(
-                  'assets/header.png',
-                  height: 120,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 30,
+                  left: 20,
+                  right: 20,
+                  bottom: 0,
                 ),
-                Text(
-                  'Boolean Algebra Quiz',
-                  style: TextStyle(
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Column(children: [
+                    Image.asset(
+                      'assets/header.png',
+                      height: 115,
+                    ),
+                    Text(
+                      'Boolean Algebra Quiz',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      ),
+                    ),
+                    Text(
+                      'Simplifying Boolean Expressions',
+                      style: TextStyle(
+                        color: const Color.fromARGB(209, 255, 255, 255),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ]),
+                ]),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 20, left: 30, right: 30),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/left-arrow.png',
+                              height: 50,
+                            ),
+                            Image.asset(
+                              'assets/algebra.png',
+                              height: 50,
+                            ),
+                            Image.asset(
+                              'assets/right-arrow.png',
+                              height: 50,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          'Question 1',
+                          style: TextStyle(
+                            color: Colors.blue.shade800,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Center(
+                            child: Opacity(
+                              opacity: 0.8,
+                              child: Text(
+                                'Boolean algebra is a branch of algebra that was formulated by _______',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(25),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                fixedSize: Size.fromWidth(400),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                backgroundColor: const Color.fromARGB(
+                                    71, 158, 158, 158), // Background color
+                              ),
+                              child: Text(
+                                'Rihanna Fenty',
+                                style: TextStyle(
+                                    color: Colors.blue.shade800,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                fixedSize: Size.fromWidth(400),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                backgroundColor: const Color.fromARGB(
+                                    71, 158, 158, 158), // Background color
+                              ),
+                              child: Text(
+                                'John Boole',
+                                style: TextStyle(
+                                    color: Colors.blue.shade800,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                fixedSize: Size.fromWidth(400),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                backgroundColor: const Color.fromARGB(
+                                    71, 158, 158, 158), // Background color
+                              ),
+                              child: Text(
+                                'George Boole',
+                                style: TextStyle(
+                                    color: Colors.blue.shade800,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                fixedSize: Size.fromWidth(400),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5)),
+                                ),
+                                backgroundColor: const Color.fromARGB(
+                                    71, 158, 158, 158), // Background color
+                              ),
+                              child: Text(
+                                'George Washington',
+                                style: TextStyle(
+                                    color: Colors.blue.shade800,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {},
+                                    style: TextButton.styleFrom(
+                                      side: BorderSide(
+                                          color: Colors.blue.shade900),
+                                      fixedSize: Size.fromWidth(200),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5)),
+                                      ),
+                                      backgroundColor: const Color.fromARGB(
+                                          71, 158, 158, 158),
+                                    ),
+                                    child: Text(
+                                      'Evaluate',
+                                      style: TextStyle(
+                                          color: Colors.blue.shade900),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  'Simplifying Boolean Expressions',
-                  style: TextStyle(
-                    color: const Color.fromARGB(209, 255, 255, 255),
-                    fontSize: 18,
-                  ),
-                ),
-              ]),
-            ]),
+              ),
+            ],
           ),
           Padding(
-            padding: EdgeInsets.all(20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
+            padding: EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/home.png',
+                  height: 40,
                 ),
-              height: 420,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('<'),
-                        Image.asset(
-                          'assets/algebra.png',
-                          height: 50,
-                        ),
-                        Text('>'),
-                      ],
-                    ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Image.asset(
+                    'assets/learn.png',
+                    height: 40,
                   ),
-                ],
-              ),
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Image.asset(
+                    'assets/quiz.png',
+                    height: 40,
+                  ),
+                ),
+                Opacity(
+                  opacity: 0.5,
+                  child: Image.asset(
+                    'assets/profile.png',
+                    height: 40,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
